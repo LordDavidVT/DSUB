d6ce434aa0d56a52f662aed0ffd63fce4e4e47fc
LordDavidVT <lorddavidvt@gmail.com>
2025-07-06 14:34:38 +0200

    Initial commit

---
+++ DSUB/DSUB github release/botInfo.hpp
@@ -0,0 +1,28 @@
+//botInfo.hpp
+#ifndef _BOTINFO_H_
+#define _BOTINFO_H_
+
+#include <string>
+
+
+class botInfo
+{
+protected:
+    
+
+public:
+   
+
+    botInfo();
+
+    ~botInfo();
+
+    std::string get_token();
+private:
+    std::string token;
+
+
+
+};
+
+#endif // _BOTINFO_H_
---
+++ DSUB/DSUB github release/messageCleaner.hpp
@@ -0,0 +1,30 @@
+//messageClearner.hpp
+#ifndef _MESSAGECLEANER_H_
+#define _MESSAGECLEANER_H_
+
+#include <vector>
+#include <string>
+#include <set>
+
+class messageCleaner
+{
+protected:
+    
+
+public:
+
+    messageCleaner();
+
+    ~messageCleaner();
+    
+    void clean_message(std::string &message_content);
+
+private:
+
+
+
+
+
+};
+
+#endif // _MESSAGECLEANER_H_
